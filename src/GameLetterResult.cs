d0703afe4d8558f08916b05aebbc640b6d5bc6c5
Jeff Handley <jeffhandley@users.noreply.github.com>
2024-06-02 22:57:11 -0700

    GameResult.Letters

--- src/GameLetterResult.cs
+++ src/GameLetterResult.cs
@@ -3,4 +3,5 @@ namespace WordleStats;
 public class GameLetterResult
 {
     public List<GuessLetterResult> GuessLetterResults { get; } = new();
+    public LetterState State => GuessLetterResults.Any() ? GuessLetterResults.Max(g => g.State) : LetterState.Unknown;
 }
---
+++ test/GameResult_Letters.cs
@@ -0,0 +1,82 @@
+namespace WordleStats.Tests;
+
+public class GameResult_Letters
+{
+    private static char[] AllLetters = "abcdefghijklmnopqrstuvwxyz".ToArray();
+
+    [Theory]
+    [InlineData("abcde", new string[] { }, "abcdefghijklmnopqrstuvwxyz")]
+    [InlineData("abcde", new string[] { "zzzzz" }, "abcdefghijklmnopqrstuvwxy")]
+    [InlineData("abcde", new string[] { "zzzzz", "uvwxy" }, "abcdefghijklmnopqrst")]
+    [InlineData("abcde", new string[] { "zzzzz", "uvwxy", "cdefg" }, "abhijklmnopqrst")]
+    [InlineData("abcde", new string[] { "zzzzz", "uvwxy", "cdefg", "bhijk" }, "almnopqrst")]
+    [InlineData("abcde", new string[] { "zzzzz", "uvwxy", "cdefg", "bhijk", "lmnop" }, "aqrst")]
+    [InlineData("abcde", new string[] { "zzzzz", "uvwxy", "cdefg", "bhijk", "lmnop", "qqrst" }, "a")]
+    [InlineData("abcde", new string[] { "zzzzz", "uvwxy", "cdefg", "bhijk", "lmnop", "qqrst", "edcba" }, "")]
+    [InlineData("abcde", new string[] { "zzzzz", "uvwxy", "cdefg", "bhijk", "lmnop", "qqrst", "edcba", "ebcda" }, "")]
+    [InlineData("abcde", new string[] { "zzzzz", "uvwxy", "cdefg", "bhijk", "lmnop", "qqrst", "edcba", "ebcda", "abcde" }, "")]
+    public void Unknown(string answer, string[] guesses, string expected)
+    {
+        var result = Game.GetGameResult(answer, guesses);
+        var actual = string.Join("", AllLetters.Where(l => result.Letters[l].State == LetterState.Unknown));
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData("abcde", new string[] { }, "")]
+    [InlineData("abcde", new string[] { "zzzzz" }, "z")]
+    [InlineData("abcde", new string[] { "zzzzz", "uvwxy" }, "uvwxyz")]
+    [InlineData("abcde", new string[] { "zzzzz", "uvwxy", "cdefg" }, "fguvwxyz")]
+    [InlineData("abcde", new string[] { "zzzzz", "uvwxy", "cdefg", "bhijk" }, "fghijkuvwxyz")]
+    [InlineData("abcde", new string[] { "zzzzz", "uvwxy", "cdefg", "bhijk", "lmnop" }, "fghijklmnopuvwxyz")]
+    [InlineData("abcde", new string[] { "zzzzz", "uvwxy", "cdefg", "bhijk", "lmnop", "qqrst" }, "fghijklmnopqrstuvwxyz")]
+    [InlineData("abcde", new string[] { "zzzzz", "uvwxy", "cdefg", "bhijk", "lmnop", "qqrst", "edcba" }, "fghijklmnopqrstuvwxyz")]
+    [InlineData("abcde", new string[] { "zzzzz", "uvwxy", "cdefg", "bhijk", "lmnop", "qqrst", "edcba", "ebcda" }, "fghijklmnopqrstuvwxyz")]
+    [InlineData("abcde", new string[] { "zzzzz", "uvwxy", "cdefg", "bhijk", "lmnop", "qqrst", "edcba", "ebcda", "abcde" }, "fghijklmnopqrstuvwxyz")]
+    public void Absent(string answer, string[] guesses, string expected)
+    {
+        var result = Game.GetGameResult(answer, guesses);
+        var actual = string.Join("", AllLetters.Where(l => result.Letters[l].State == LetterState.Absent));
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData("abcde", new string[] { }, "")]
+    [InlineData("abcde", new string[] { "zzzzz" }, "")]
+    [InlineData("abcde", new string[] { "zzzzz", "uvwxy" }, "")]
+    [InlineData("abcde", new string[] { "zzzzz", "uvwxy", "cdefg" }, "cde")]
+    [InlineData("abcde", new string[] { "zzzzz", "uvwxy", "cdefg", "bhijk" }, "bcde")]
+    [InlineData("abcde", new string[] { "zzzzz", "uvwxy", "cdefg", "bhijk", "lmnop" }, "bcde")]
+    [InlineData("abcde", new string[] { "zzzzz", "uvwxy", "cdefg", "bhijk", "lmnop", "qqrst" }, "bcde")]
+    [InlineData("abcde", new string[] { "zzzzz", "uvwxy", "cdefg", "bhijk", "lmnop", "qqrst", "edcba" }, "abde")]
+    [InlineData("abcde", new string[] { "zzzzz", "uvwxy", "cdefg", "bhijk", "lmnop", "qqrst", "edcba", "ebcda" }, "ae")]
+    [InlineData("abcde", new string[] { "zzzzz", "uvwxy", "cdefg", "bhijk", "lmnop", "qqrst", "edcba", "ebcda", "abcde" }, "")]
+    public void Present(string answer, string[] guesses, string expected)
+    {
+        var result = Game.GetGameResult(answer, guesses);
+        var actual = string.Join("", AllLetters.Where(l => result.Letters[l].State == LetterState.Present));
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData("abcde", new string[] { }, "")]
+    [InlineData("abcde", new string[] { "zzzzz" }, "")]
+    [InlineData("abcde", new string[] { "zzzzz", "uvwxy" }, "")]
+    [InlineData("abcde", new string[] { "zzzzz", "uvwxy", "cdefg" }, "")]
+    [InlineData("abcde", new string[] { "zzzzz", "uvwxy", "cdefg", "bhijk" }, "")]
+    [InlineData("abcde", new string[] { "zzzzz", "uvwxy", "cdefg", "bhijk", "lmnop" }, "")]
+    [InlineData("abcde", new string[] { "zzzzz", "uvwxy", "cdefg", "bhijk", "lmnop", "qqrst" }, "")]
+    [InlineData("abcde", new string[] { "zzzzz", "uvwxy", "cdefg", "bhijk", "lmnop", "qqrst", "edcba" }, "c")]
+    [InlineData("abcde", new string[] { "zzzzz", "uvwxy", "cdefg", "bhijk", "lmnop", "qqrst", "edcba", "ebcda" }, "bcd")]
+    [InlineData("abcde", new string[] { "zzzzz", "uvwxy", "cdefg", "bhijk", "lmnop", "qqrst", "edcba", "ebcda", "abcde" }, "abcde")]
+    public void Correct(string answer, string[] guesses, string expected)
+    {
+        var result = Game.GetGameResult(answer, guesses);
+        var actual = string.Join("", AllLetters.Where(l => result.Letters[l].State == LetterState.Correct));
+
+        Assert.Equal(expected, actual);
+    }
+}
